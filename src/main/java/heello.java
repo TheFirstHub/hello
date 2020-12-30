4125ee4d87f99b197a91f873fe27a1b8d1c5e4ff
TheFirstHub <2753162841@qq.com>
2020-12-30 14:16:31 +0800

    hello

---
+++ src/main/java/heello.java
@@ -0,0 +1,11 @@
+/**
+ * @author mengfanmei
+ * @version 1.0
+ * @Description
+ * @Date 2020/12/30
+ */
+public class heello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
